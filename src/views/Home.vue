e418775b422fecba6c5f83b5d0d3859b37ca2e1c
abderaouf <abderaouf.mokrane@nassidj.com>
2019-03-13 13:20:51 +0100

    route

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <dashboard msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import dashboard from '@/components/dashboard'
+
+export default {
+  name: 'home',
+  components: {
+    dashboard
+  }
+}
+</script>
